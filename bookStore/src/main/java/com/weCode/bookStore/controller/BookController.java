f920877ea7f7618628a576ae894e9e7dc55f050c
linyu <z5135225@ad.unsw.edu.au>
2022-10-12 03:39:07 -0700

    Initial commit

---
+++ bookStore/src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.controller.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(("api/v1/books"))
+public class BookController {
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getbooks(){
+        BookDto book = BookDto.builder()
+                .title("My First book title")
+                .build();
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+
+    }
+}
---
+++ bookStore/src/main/java/com/weCode/bookStore/controller/dto/BookDto.java
@@ -0,0 +1,16 @@
+package com.weCode.bookStore.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    private  String title;
+
+
+}
